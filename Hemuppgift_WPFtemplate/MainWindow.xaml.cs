6e9c56df71de3f260391c81f1b6c0522c4ff1fc3
audioken <kenzachrisson@gmail.com>
2024-10-03 12:24:28 +0200

    Skapat MainWindow med fyra knappar för Grid, StackPanel, WrapPanel och DockPanel. "Click"-metoder är skapade för varje knapp.

--- Hemuppgift_WPFtemplate/MainWindow.xaml.cs
+++ Hemuppgift_WPFtemplate/MainWindow.xaml.cs
@@ -2,15 +2,31 @@
 
 namespace Hemuppgift_WPFtemplate
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        //HEJ
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnGrid_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnStackPanel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnWrapPanel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDockPanel_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
