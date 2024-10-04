0154be37361f06b06db53724c31485c537e90ed8
audioken <kenzachrisson@gmail.com>
2024-10-04 21:38:42 +0200

    Rättade till värdena vid inmatning i GridWindow till mellan 0-3 istället för 0-4. Tog också bort onödig kod.

--- Hemuppgift_WPFtemplate/GridWindow.xaml.cs
+++ Hemuppgift_WPFtemplate/GridWindow.xaml.cs
@@ -15,7 +15,7 @@ namespace Hemuppgift_WPFtemplate
         {
             // Kontrollerar att båda inmatningarna är siffror mellan 0-4
             if (int.TryParse(txtRow.Text, out int row) && int.TryParse(txtColumn.Text, out int column)
-                && row >= 0 && row <= 4 && column >= 0 && column <= 4)
+                && row >= 0 && row <= 3 && column >= 0 && column <= 3)
             {
                 // Uppdaterar knappens text med koordinaterna
                 btnPosition.Content = $"Row {row}, Column {column}";
@@ -23,14 +23,11 @@ namespace Hemuppgift_WPFtemplate
                 // Placerar knappen enligt koordinaterna
                 Grid.SetRow(btnPosition, row + 1); // Justerar eftersom StackPanel redan ligger i row 0
                 Grid.SetColumn(btnPosition, column);
-
-                // Visar knappen i grid
-                btnPosition.Visibility = Visibility.Visible;
             }
             else
             {
                 // Visar felmeddelande vid felaktig inmatning
-                MessageBox.Show("Use digits between 0-4 only");
+                MessageBox.Show("Use digits between 0-3 only");
             }
         }
     }
